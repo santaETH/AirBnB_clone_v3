770ed89b3bcf593f2358c06ae192b69109adf3d2
Natnael Seifu <natiseifu02@gmail.com>
2024-05-27 13:49:41 -0100

    fix bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -54,7 +54,9 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                cls_name = jo[key]["__class__"]
+                obj = classes[cls_name](**jo[key])  # Create instance
+                self.__objects[key] = obj
         except Exception:
             pass
 
