3dab018277130777658669e23401a56a89506989
santaETH <sdamissie@yahoo.com>
2024-05-27 23:15:44 +0300

    fix 2

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script that imports a Blueprint and runs Flask """
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """it closes the storage session """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
