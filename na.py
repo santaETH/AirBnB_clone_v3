ff4fab63c842648e9827f502d72454ff5b6f3b8c
Natnael Seifu <natiseifu02@gmail.com>
2024-05-29 21:14:22 -0100

    fix

--- na.py
+++
@@ -1,5 +0,0 @@
-def test_states_endpoint(self):
-    """Test that states endpoint returns a list of states"""
-    r = requests.get("http://0.0.0.0:5050/api/v1/states")
-    self.assertEqual(r.status_code, 200)
-    self.assertIsInstance(r.json(), list)
